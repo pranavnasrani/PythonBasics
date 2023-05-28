bbf9cac87a461cf9c4ccbc934acba587cccad136
Pranav Asrani <pranavnasrani@outlook.sg>
2023-05-28 08:17:57 +0800

    Modified PythonBasics.py main.py deleted Additional basic practice code added in PythonBasics.py
    Covered Sample String
    Booleans
    Strings
    and Inputted String

---
+++ PythonBasics.py
@@ -0,0 +1,19 @@
+# Sample Printing String
+print("Hello world from Python")
+
+# Booleans and printing
+result = 3 < 10
+print(type(result))
+print(result)
+
+# Strings
+name = "Pranav Asrani"
+print(name[0:2])
+
+# Concatenation of inputted string
+first_name1 = input("What is your first name? ")
+last_name2 = input("What is your last name? ")
+result = first_name1 + " " + last_name2
+print(result)
+
+
