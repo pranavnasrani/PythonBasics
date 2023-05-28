b4179f9d44e77fa22a1f5738e4f733f58e8fd683
Pranav Asrani <pranavnasrani@outlook.sg>
2023-05-29 07:24:52 +0800

    Edited loops.py

---
+++ loops.py
@@ -0,0 +1,16 @@
+# FOR LOOPS
+name = "Kevin"
+
+for letter in name:
+    print(letter)
+else:
+    print("the letters were printed out")
+
+index = 0
+while index < len(name):
+    print(name[index])
+    index += 1
+else:
+    print('Finished with the while loop.')
+
+
